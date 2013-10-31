0d9e129b41a3ea7252f4f2e8c3bbbe52a28eb3a3
Leah Hanson <astrieanna@gmail.com>
2013-10-31 12:41:09 -0400

    shared list between 2 workers; made channels more complicated, but it works.

--- pmap.rs
+++ pmap.rs
@@ -1,19 +1,22 @@
 extern mod extra;
-use extra::comm::DuplexStream;
+extern mod std;
+use std::rand;
 
 fn sqr(x:int) -> int {
 	return x*x;
 }
 
 
-fn pmap(fun: extern fn(~str) -> uint, myvect:&[~str]) {
-	let (child1, parent1) = DuplexStream();
-	let (child2, parent2) = DuplexStream();
-
+fn pmap(fun: extern fn(~str) -> uint, myvect:~[~str]) {
+        let (parent_port1, child_chan1) = stream();
+        let (child_port1, parent_chan1) = stream();
+        let (parent_port2, child_chan2) = stream();
+        let (child_port2, parent_chan2) = stream();
+        
 	do spawn {
 		loop {
-                        match child1.try_recv() {
-			    Some(s) => child1.send(fun(s)),
+                        match child_port1.try_recv() {
+			    Some(s) => child_chan1.send(fun(s)),
                             None => break
                         }
 		}
@@ -21,21 +24,41 @@ fn pmap(fun: extern fn(~str) -> uint, myvect:&[~str]) {
 
 	do spawn {
 		loop {
-                        match child2.try_recv() {
-			    Some(s) => child2.send(fun(s)),
+                        match child_port2.try_recv() {
+			    Some(s) => child_chan2.send(fun(s)),
                             None => break
                         }
 		}
 	}
 
+        do spawn {
 	for iptr in myvect.iter() {
 		let s = (*iptr).to_owned();
 		let t = s.clone();
-		parent1.send(s);
-		parent2.send(t);
-		println(format!("{:u}", parent1.recv()));
-		println(format!("{:u}", parent2.recv()));
+                let i = rand::random::<uint>();
+		
+                match i % 2 {
+                  0 => parent_chan1.send(s),
+		  1 => parent_chan2.send(t),
+                  j => fail!("{:u} % 2 = {:u}",i,j)
+                }
 	}
+        }
+
+        println("working on parent1");
+        loop {
+          match parent_port1.try_recv() {
+           Some(v) => println(format!("{:u}",v)),
+           None => break
+          }
+        }
+        println("working on parent2");
+        loop {
+          match parent_port2.try_recv() {
+           Some(v) => println(format!("{:u}",v)),
+           None => break
+          }
+        }
 }
 
 fn get_length(x:~str) -> uint {
@@ -43,7 +66,7 @@ fn get_length(x:~str) -> uint {
 }
 
 fn main() {
-	let myvect: &[~str] = &[~"Alex", ~"Julia", ~"Yaron"];
+	let myvect: ~[~str] = ~[~"Alex", ~"Julia", ~"Yaron"];
 	pmap(get_length, myvect);
 }
 
